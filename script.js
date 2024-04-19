eb1385b148d3eb08e1c741bc32deabc36f32c4a2
Dheeraj Shrivastav <126507107+Doreamo-max@users.noreply.github.com>
2024-04-19 23:22:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const input = document.getElementById("input-box");
+const list = document.getElementById("list-container");
+function addTask() {
+    if (input.value === '') {
+        alert("You must write something");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        list.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML=" &#x274c"
+        li.appendChild(span);
+        
+    }
+    input.value = "";
+    saveData();
+}
+
+list.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+function saveData() {
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showData(){
+    list.innerHTML =localStorage.getItem("data");
+}
+showData();
